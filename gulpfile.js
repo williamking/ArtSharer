ed71a4c01dc41ddbfc3063b449d1023ea368b7b7
william <williamjwking@gmail.com>
2016-02-24 21:41:07 +0800

    try to build the UI of the editor

--- gulpfile.js
+++ gulpfile.js
@@ -10,10 +10,38 @@ gulp.task("webpack", function() {
 		.pipe(livereload());
 });
 
+gulp.task("webpack2", function() {
+    gulp.src('./')
+    .pipe(webpack({
+        watch: true,
+        entry: {
+            "editor": __dirname + '/public/components/editor.js'
+        },
+        output: {
+            path: __dirname + 'public/dist',
+            filename: '[name].bundle.js',
+        },
+        module: {
+            loaders: [
+                { test: /\.js$/, loader: 'babel-loader!jsx-loader?harmony', exclude: /node_modules/ },
+                { test: /\.css$/, loader: 'style!css' }
+            ],
+            postLoaders: [
+                { loader: 'transform?brfs' }
+            ]
+        },
+        resolve: {
+            extensions: ['', '.js', '.jsx']
+        }
+    }))
+    .pipe(gulp.dest('./public/dist'))
+    .pipe(livereload());
+});
+
 gulp.task("watch", function() {
 	livereload.listen();
 });
 
 gulp.task("default", function () {
-	gulp.start("webpack", "watch");
+	gulp.start("webpack", "webpack2", "watch");
 });
