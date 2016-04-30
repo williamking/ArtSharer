f47059df78f5a61105dc5f1d4dc799803b023eb5
william <williamjwking@gmail.com>
2016-04-30 18:23:51 +0800

    test

--- public/react_view/index_v.js
+++ public/react_view/index_v.js
@@ -8,4 +8,40 @@ var ReactDOM = require('react-dom');
 var CurrentArtoworkList = React.createClass({
 
     getInitialState: function() {
+        return {
+            currentList: [
+            {title:'work-1', created: '2016-12-13'},
+            {title:'work-1', created: '2016-12-13'},
+            {title:'work-1', created: '2016-12-13'},
+            {title:'work-1', created: '2016-12-13'},
+            {title:'work-1', created: '2016-12-13'}
+            ]
+        }
+    },
+    
+    render: function() {
+        var items = this.state.currentList.map(function(item, key) {
+            return (
+                <div className="item">
+                    <div className="content">
+                        <a className="header">{item.title}</a>
+                        <div className="description">Created at {item.created}</div>
+                    </div>
+                </div>
+            );
+        };
+        return (
+            <div id="current-artworks">
+                <header>
+                    <h3 className="ui h3">Current Artworks</h3>
+                    <button className="ui green button"></button>
+                </header>
+                <div id="current-artworks-list" className="ui relaxed divided list">
+                    {items}       
+                </div>
+            </div>
+        );
+    }
+});
+
 
